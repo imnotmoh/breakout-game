ebd572278880f3496d2efdd17c96fbe309d48e86
imnotmoh <122214693+imnotmoh@users.noreply.github.com>
2023-03-08 21:23:38 +0100

    a better version

--- game_obj.py
+++ game_obj.py
@@ -1,13 +1,15 @@
 import random
 from turtle import Turtle, Screen
 
+pace = 3
 class Paddle(Turtle):
-    def __init__(self):
+    def __init__(self, ball):
         super().__init__()
+        self.ball = ball
         self.penup()
         self.shapesize(stretch_wid=1, stretch_len=6)
         self.shape("square")
-        self.goto(x=0,y=-300)
+        self.goto(x=0, y=-300)
 
     def move_right(self):
         if self.xcor() + 60 < 280:
@@ -17,9 +19,12 @@ class Paddle(Turtle):
         if self.xcor() - 60 > -280:
             self.backward(20)
 
-
-
-
+    def contact(self):
+        if self.ycor() + 15 >= self.ball.ycor() >=self.ycor():
+            if self.xcor() - 60 <= self.ball.xcor() <= self.xcor():
+                self.ball.setheading(90 -(-self.xcor() + self.ball.xcor()))
+            if self.xcor() + 60 >= self.ball.xcor() >= self.xcor():
+                self.ball.setheading(90 -(-self.xcor() + self.ball.xcor()))
 
 
 
@@ -30,26 +35,70 @@ class Blocks(Turtle):
         self.speed("fastest")
         self.penup()
         self.shape("square")
-        self.shapesize(stretch_len=2,stretch_wid=1)
+        self.shapesize(stretch_len=2, stretch_wid=1)
 
-color = ["red","yellow", "purple", "green", "blue", "pink", "brown"]
 
-block_list = []
-def arrangeblocks(screen_width, screen_height):
-    x = -(screen_width/2) + 40
-    y = (screen_height/2) - 20
+color = ["red", "yellow", "purple", "green", "blue", "pink", "brown"]
 
-    for i in range(int((screen_width - 40) /40) * 5):
-        block_list.append(Blocks())
-        block_list[i].goto(x,y)
-        block_list[i].pensize(5)
-        block_list[i].color("black")
-        block_list[i].fillcolor(random.choice(color))
 
-        x += 40
-        if x > (screen_width/2) - 40:
-            x = -(screen_width/2) + 40
-            y -= 20
+class ArrangedBlocks():
+    def __init__(self, ball):
+        self.block_list = []
+        self.ball = ball
+
+    def arrangeblocks(self, screen_width, screen_height):
+
+        x = -(screen_width / 2) + 40
+        y = (screen_height / 2) - 20
+
+        for i in range(int((screen_width - 40) / 40) * 5):
+            self.block_list.append(Blocks())
+            self.block_list[i].goto(x, y)
+            self.block_list[i].pensize(5)
+            self.block_list[i].color("black")
+            self.block_list[i].fillcolor(random.choice(color))
+
+            x += 40
+            if x > (screen_width / 2) - 40:
+                x = -(screen_width / 2) + 40
+                y -= 20
+
+    def block_horizontal_contact(self):
+        for block in self.block_list:
+            if block.ycor() - 15 <= self.ball.ycor() <= block.ycor():
+                pos_prox = block.xcor() - self.ball.xcor()
+                if -20 <= pos_prox <= 20:
+                    block.hideturtle()
+                    print("downtouch")
+                    self.block_list.remove(block)
+                    self.ball.ball_physics()
+                    self.ball.forward(pace)
+                    return True
+
+            if block.ycor() + 15 >= self.ball.ycor() >= block.ycor():
+                pos_prox = block.xcor() - self.ball.xcor()
+                if -20 <= pos_prox <= 20:
+                    block.hideturtle()
+                    self.block_list.remove(block)
+                    self.ball.ball_physics()
+                    self.ball.forward(pace)
+                    return True
+
+    def block_side_contact(self):
+        for block in self.block_list:
+            if block.ycor() - 10 < self.ball.ycor() < block.ycor() + 10:
+                if block.xcor() + 21 > self.ball.xcor() > block.xcor() and block.distance(self.ball) < 26:
+                    print("right touch")
+                    block.hideturtle()
+                    self.block_list.remove(block)
+                    self.ball.wall_bounce_physics()
+                    self.ball.forward(pace)
+                elif block.xcor() - 21 < self.ball.xcor() < block.xcor() and block.distance(self.ball) < 26:
+                    block.hideturtle()
+                    self.block_list.remove(block)
+                    print("left touch")
+                    self.ball.wall_bounce_physics()
+                    self.ball.forward(pace)
 
 
 class Ball(Turtle):
@@ -57,11 +106,11 @@ class Ball(Turtle):
         super().__init__()
         self.color("black")
         self.shape("circle")
+        self.shapesize(stretch_wid=0.5, stretch_len=0.5)
         self.penup()
-        self.goto(random.randint(0,50), -280)
-        self.setheading(random.randint(45,150))
+        self.goto(0, -300)
+        self.setheading(random.randint(45, 150))
         self.speed("fastest")
-
 
     def ball_physics(self):
         direction = self.heading()
@@ -72,18 +121,3 @@ class Ball(Turtle):
         direction = self.heading()
         bounce_angle = 180 - direction
         self.setheading(bounce_angle)
-
-    def block_side_bounce(self):
-        if self.heading() < 90 or self.heading() < 270:
-            self.setheading(self.heading() + 45)
-        elif self.heading() > 90 or self.heading() > 270:
-            self.setheading(self.heading() - 45)
-
-
-
-
-
-
-
-
-
--- main.py
+++ main.py
@@ -4,64 +4,34 @@ from game_obj import *
 my_screen = Screen()
 my_screen.tracer(0)
 my_screen.setup(width=600,height=700)
-arrangeblocks(600,700)
+pace = 3
 ball = Ball()
-pad = Paddle()
+
+setup = ArrangedBlocks(ball)
+setup.arrangeblocks(600,700)
+# setup.block_list[5].goto(50,50)
+pad = Paddle(ball)
 game_is_on = True
+
+block_list = []
 while game_is_on:
-    time.sleep(0.0001)
-    for block in block_list:
-        try:
-            if ball.distance(block) < 35 and block.ycor() - 35 < ball.ycor():
-                if 20 < block.distance(block_list[block_list.index(block) + 1]) > 40 or 20 < block.distance(block_list[block_list.index(block) - 1]) > 40:
-                    print(1)
-                    if (ball.distance(block) < 20 and block.xcor() + 20 > ball.xcor()) or (ball.distance(block) < 20 and block.xcor() - 20 < ball.xcor()):
-                        print(2)
-                        if ball.ycor() + 5 > block.ycor():
-                            print("hello")
-                            ball.block_side_bounce()
-                            ball.forward(10)
-                            block.hideturtle()
-                            block_list.remove(block)
-                    else:
-                        ball.ball_physics()
-                        ball.forward(10)
-                        block_list.remove(block)
-                        block.hideturtle()
-                        print("na me")
-                else:
-                    ball.ball_physics()
-                    ball.forward(10)
-                    block_list.remove(block)
-                    block.hideturtle()
-                    print("na me")
-        except IndexError:
-
-
-            if ball.distance(block) < 25 and block.ycor() - 25 < ball.ycor():
-                ball.ball_physics()
-                ball.forward(10)
-                block_list.remove(block)
-                block.hideturtle()
-            elif 20 < block.distance(block_list[block_list.index(block) - 1]) > 40:
-                if (ball.distance(block) < 25 and block.xcor() - 25 < ball.ycor()) or (ball.distance(block) < 25 and block.xcor() + 25 > ball.ycor()):
-                    ball.block_side_bounce()
-                    ball.forward(10)
-                    block.hideturtle()
-                    block_list.remove(block)
-
-    if pad.ycor() + 10 > ball.ycor() and ball.distance(pad) < 60:
-        if ball.ycor() > pad.ycor():
-            ball.ball_physics()
-    if ball.xcor() > 280 or ball.xcor() < - 280:
+    ball.forward(pace)
+    setup.block_side_contact()
+    setup.block_horizontal_contact()
+    my_screen.update()
+    # if ball.ycor() < -240:
+    #     ball.ball_physics()
+    if -265 < ball.xcor() < 280:
+        pass
+    else:
         ball.wall_bounce_physics()
+    pad.contact()
     if ball.ycor() > 330:
         ball.ball_physics()
-    ball.forward(5)
-    my_screen.update()
-    if ball.ycor() < -330:
-        ball.goto(0,-400)
+    elif ball.ycor() < - 360:
+        ball.color("white")
         ball.__init__()
+
     my_screen.listen()
     my_screen.onkey(key="Right",fun=pad.move_right)
     my_screen.onkey(key="Left",fun=pad.move_left)
