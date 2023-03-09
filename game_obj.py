1f8a42cb74b00a4b7347ab2ff6c9fdec441c48a2
imnotmoh <122214693+imnotmoh@users.noreply.github.com>
2023-03-09 16:13:15 +0100

    improved gameplay

--- game_obj.py
+++ game_obj.py
@@ -1,7 +1,8 @@
 import random
 from turtle import Turtle, Screen
 
-pace = 3
+pace = 0
+
 class Paddle(Turtle):
     def __init__(self, ball):
         super().__init__()
@@ -9,6 +10,7 @@ class Paddle(Turtle):
         self.penup()
         self.shapesize(stretch_wid=1, stretch_len=6)
         self.shape("square")
+        self.color("white")
         self.goto(x=0, y=-300)
 
     def move_right(self):
@@ -23,8 +25,12 @@ class Paddle(Turtle):
         if self.ycor() + 15 >= self.ball.ycor() >=self.ycor():
             if self.xcor() - 60 <= self.ball.xcor() <= self.xcor():
                 self.ball.setheading(90 -(-self.xcor() + self.ball.xcor()))
+                if self.ball.speed_ > 0:
+                    self.ball.speed_ -= 0.001
             if self.xcor() + 60 >= self.ball.xcor() >= self.xcor():
                 self.ball.setheading(90 -(-self.xcor() + self.ball.xcor()))
+                if self.ball.speed_ > 0:
+                    self.ball.speed_ -= 0.001
 
 
 
@@ -45,13 +51,14 @@ class ArrangedBlocks():
     def __init__(self, ball):
         self.block_list = []
         self.ball = ball
+        self.level = 1
 
     def arrangeblocks(self, screen_width, screen_height):
 
         x = -(screen_width / 2) + 40
         y = (screen_height / 2) - 20
 
-        for i in range(int((screen_width - 40) / 40) * 5):
+        for i in range(int((screen_width - 40) / 40) * (4+self.level)):
             self.block_list.append(Blocks())
             self.block_list[i].goto(x, y)
             self.block_list[i].pensize(5)
@@ -64,16 +71,18 @@ class ArrangedBlocks():
                 y -= 20
 
     def block_horizontal_contact(self):
+        global speed
         for block in self.block_list:
             if block.ycor() - 15 <= self.ball.ycor() <= block.ycor():
                 pos_prox = block.xcor() - self.ball.xcor()
                 if -20 <= pos_prox <= 20:
                     block.hideturtle()
-                    print("downtouch")
+                    if self.ball.speed_ > 0:
+                        self.ball.speed_ -= 0.001
                     self.block_list.remove(block)
                     self.ball.ball_physics()
                     self.ball.forward(pace)
-                    return True
+
 
             if block.ycor() + 15 >= self.ball.ycor() >= block.ycor():
                 pos_prox = block.xcor() - self.ball.xcor()
@@ -82,35 +91,41 @@ class ArrangedBlocks():
                     self.block_list.remove(block)
                     self.ball.ball_physics()
                     self.ball.forward(pace)
+                    if self.ball.speed_ > 0:
+                        self.ball.speed_ -= 0.001
                     return True
 
     def block_side_contact(self):
+        global speed
         for block in self.block_list:
             if block.ycor() - 10 < self.ball.ycor() < block.ycor() + 10:
                 if block.xcor() + 21 > self.ball.xcor() > block.xcor() and block.distance(self.ball) < 26:
-                    print("right touch")
                     block.hideturtle()
                     self.block_list.remove(block)
                     self.ball.wall_bounce_physics()
                     self.ball.forward(pace)
+                    if self.ball.speed_ > 0:
+                        self.ball.speed_ -= 0.001
                 elif block.xcor() - 21 < self.ball.xcor() < block.xcor() and block.distance(self.ball) < 26:
                     block.hideturtle()
                     self.block_list.remove(block)
-                    print("left touch")
                     self.ball.wall_bounce_physics()
                     self.ball.forward(pace)
+                    if self.ball.speed_ > 0:
+                        self.ball.speed_ -= 0.001
 
 
 class Ball(Turtle):
     def __init__(self):
         super().__init__()
-        self.color("black")
+        self.color("white")
         self.shape("circle")
         self.shapesize(stretch_wid=0.5, stretch_len=0.5)
         self.penup()
         self.goto(0, -300)
         self.setheading(random.randint(45, 150))
-        self.speed("fastest")
+        self.speed_ = 0.01
+        self.speed(0)
 
     def ball_physics(self):
         direction = self.heading()
@@ -121,3 +136,13 @@ class Ball(Turtle):
         direction = self.heading()
         bounce_angle = 180 - direction
         self.setheading(bounce_angle)
+
+
+
+class GameOver(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.hideturtle()
+        self.goto(0,0)
+        self.color("white")
+        self.write("gameover")
--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import time
 from turtle import Turtle, Screen
 from game_obj import *
 my_screen = Screen()
+my_screen.bgcolor("black")
 my_screen.tracer(0)
+lives = 5
+
 my_screen.setup(width=600,height=700)
-pace = 3
+pace = 5
 ball = Ball()
 
 setup = ArrangedBlocks(ball)
@@ -12,16 +15,17 @@ setup.arrangeblocks(600,700)
 # setup.block_list[5].goto(50,50)
 pad = Paddle(ball)
 game_is_on = True
-
 block_list = []
 while game_is_on:
+
+    time.sleep(ball.speed_)
     ball.forward(pace)
     setup.block_side_contact()
     setup.block_horizontal_contact()
     my_screen.update()
     # if ball.ycor() < -240:
     #     ball.ball_physics()
-    if -265 < ball.xcor() < 280:
+    if -270 < ball.xcor() < 280:
         pass
     else:
         ball.wall_bounce_physics()
@@ -30,7 +34,17 @@ while game_is_on:
         ball.ball_physics()
     elif ball.ycor() < - 360:
         ball.color("white")
-        ball.__init__()
+        if lives > 0:
+            ball.__init__()
+            lives -= 1
+        else:
+            game_over = GameOver()
+            break
+
+
+    if len(setup.block_list) < 1:
+        setup.level += 1
+        setup.arrangeblocks(600,700)
 
     my_screen.listen()
     my_screen.onkey(key="Right",fun=pad.move_right)
